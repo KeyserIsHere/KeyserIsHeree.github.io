64d47972537ae9ab0be6fe5e1f2dcf54308c6da1
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-09-06 19:03:14 -0300

    Accept multiple cats
    MEOW 🐱! Edited app.js to accept multiple cats using the Cat class (now we have Kimmy and Logan)

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
-var count = 0;
+class Cat {
+    constructor(name, picture) {
+        let self = this;
 
-function update() {
-	var counter = $('#counter')
+        self.name = name;
+        self.picture = picture;
+        self.count = 0;
 
-	counter.text(`You clicked the cat ${count} times.`);
-	counter.css("font-size", "+=0.5");
+        let mainDiv = $('#cats');
+        let catDiv = $('<div></div>');
+
+        mainDiv.append(catDiv);
+
+        self.img = $(`<img src="${self.picture}">`);
+        catDiv.append(self.img);
+
+        self.counter = $('<p></p>');
+        catDiv.append(self.counter);
+
+        self.img.click(function () {
+            self.count++;
+            self.counter.text(`You clicked ${self.name} ${self.count} times.`);
+            self.counter.css("font-size", "+=0.5");
+        });
+    }
 }
 
-$(document).ready(function() {
-	update();
-
-	$('img').click(function() {
-		count++;
-		update();
-	});
+$(document).ready(function () {
+    new Cat('Kimmy', 'img/cat.jpg');
+    new Cat('Logan', 'img/cat2.jpg');
 });
