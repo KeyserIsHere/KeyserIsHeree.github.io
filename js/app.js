c8cafbfd92ac8eac4e7b11a9a6003c6fada909d4
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-09-07 15:08:57 -0300

    Fix mixed tab and spaces

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
 function hideElement(selector) {
-	selector.css('display', 'none');
+    selector.css('display', 'none');
 }
 
 function showElement(selector) {
-	selector.css('display', '');
+    selector.css('display', '');
 }
 
 function createCat(name, picture) {
-	let clickCount = 0;
+    let clickCount = 0;
 
-	const $cat = $('<div />', {class: 'cat'});
-	const $img = $('<img />', {src: picture}).appendTo($cat);
-	const $counter = $('<p />').appendTo($cat);
-	const $menuItem = $(`<li><a href="#">${name}</a></li>`).appendTo($('#menu'));
+    const $cat = $('<div />', {class: 'cat'});
+    const $img = $('<img />', {src: picture}).appendTo($cat);
+    const $counter = $('<p />').appendTo($cat);
+    const $menuItem = $(`<li><a href="#">${name}</a></li>`).appendTo($('#menu'));
 
-	$img.click(() => {
-		clickCount++;
+    $img.click(() => {
+        clickCount++;
 		
-		$counter
-		  .text(`You clicked ${name} ${clickCount} times!`)
-		  .css('font-size', '+=0.25');
-	});
+        $counter
+            .text(`You clicked ${name} ${clickCount} times!`)
+            .css('font-size', '+=0.25');
+    });
 
-	$menuItem.click((e) => {
-		hideElement($('.cat'));
+    $menuItem.click((e) => {
+        hideElement($('.cat'));
 
-		e.preventDefault();
-		showElement($cat);
-	});
+        e.preventDefault();
+        showElement($cat);
+    });
 
-	return $cat;
+    return $cat;
 }
 
 $(function() {
-	const $main = $('#cats');
+    const $main = $('#cats');
 
-	$main.append(createCat('Kimmy', 'img/cat.jpg'));
-	$main.append(createCat('Logan', 'img/cat2.jpg'));
-	$main.append(createCat('Bella & Tigger', 'img/cat3.jpg'));
-	$main.append(createCat('Luna', 'img/cat4.jpg'));
+    $main.append(createCat('Kimmy', 'img/cat.jpg'));
+    $main.append(createCat('Logan', 'img/cat2.jpg'));
+    $main.append(createCat('Bella & Tigger', 'img/cat3.jpg'));
+    $main.append(createCat('Luna', 'img/cat4.jpg'));
 
-	hideElement($('.cat'));
+    hideElement($('.cat'));
 });
