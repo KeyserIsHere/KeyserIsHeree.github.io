515aed211ded92529eb5c670700c86a3c2873df5
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2017-09-07 09:35:44 -0300

    Make code more idiomatic
    Delete the class Cat and create a method to display and handle the pets

--- js/app.js
+++ js/app.js
@@ -1,46 +1,44 @@
-class Cat {
-    constructor(name, picture) {
-        let self = this;
-
-        self.name = name;
-        self.picture = picture;
-        self.count = 0;
-
-        self.menuItem = $(`<li><a href="#">${self.name}</a></li>`);
-        $("ul").append(self.menuItem);
-
-        let mainDiv = $('#cats');
-        let catDiv = $('<div class="cat"></div>');
-        catDiv.css("display", "none");
-
-        self.img = $(`<img src="${self.picture}">`);
-        self.counter = $('<p></p>');
-
-        catDiv.append(self.img);
-        catDiv.append(self.counter);
-
-        self.menuItem.click(function(e) {
-            e.preventDefault();
-
-            // hide all others
-            $(".cat").css("display", "none");
-
-            // display the selected
-            catDiv.css("display", "");
-            mainDiv.append(catDiv);
-        });
-
-        self.img.click(function () {
-            self.count++;
-            self.counter.text(`You clicked ${self.name} ${self.count} times.`);
-            self.counter.css("font-size", "+=0.5");
-        });
-    }
+function hideElement(selector) {
+	selector.css('display', 'none');
 }
 
-$(document).ready(function () {
-    new Cat('Kimmy', 'img/cat.jpg');
-    new Cat('Logan', 'img/cat2.jpg');
-    new Cat('Bella & Tigger', 'img/cat3.jpg');
-    new Cat('Luna', 'img/cat4.jpg');
+function showElement(selector) {
+	selector.css('display', '');
+}
+
+function createCat(name, picture) {
+	let clickCount = 0;
+
+	const $cat = $('<div />', {class: 'cat'});
+	const $img = $('<img />', {src: picture}).appendTo($cat);
+	const $counter = $('<p />').appendTo($cat);
+	const $menuItem = $(`<li><a href="#">${name}</a></li>`).appendTo($('#menu'));
+
+	$img.click(() => {
+		clickCount++;
+		
+		$counter
+		  .text(`You clicked ${name} ${clickCount} times!`)
+		  .css('font-size', '+=0.25');
+	});
+
+	$menuItem.click((e) => {
+		hideElement($('.cat'));
+
+		e.preventDefault();
+		showElement($cat);
+	});
+
+	return $cat;
+}
+
+$(function() {
+	const $main = $('#cats');
+
+	$main.append(createCat('Kimmy', 'img/cat.jpg'));
+	$main.append(createCat('Logan', 'img/cat2.jpg'));
+	$main.append(createCat('Bella & Tigger', 'img/cat3.jpg'));
+	$main.append(createCat('Luna', 'img/cat4.jpg'));
+
+	hideElement($('.cat'));
 });
